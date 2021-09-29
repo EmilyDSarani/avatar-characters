f9fa3075f12c521f6fbecb63690b849c8d18374a
EmilyDSarani <emily.sarani@gmail.com>
2021-09-29 15:24:52 -0500

    avatar.js done

---
+++ data/avatar.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    name:'Aang',
+    element: 'Air',
+    img: 'https://i.insider.com/5ebdbc8c3ad8612a1c7aa143?width=856&format=jpeg',
+    title: 'Avatar' 
+  }, 
+  {
+    name:'Zuko',
+    element: 'Fire',
+    img: 'https://i.redd.it/jcyddoyzeo761.jpg',
+    title: 'Prince' 
+  }, 
+  {
+    name:'Katara',
+    element: 'Water',
+    img: 'http://pm1.narvii.com/6078/caabea330dc68f44a3f855606f897516ec582759_00.jpg',
+    title: 'Healer' 
+  }, 
+  {
+    name:'Sokka',
+    element: 'None',
+    img: 'https://i.pinimg.com/originals/08/ca/e8/08cae8f53ebfd473b1b3133b0138d9cd.jpg',
+    title: 'Plan-Guy' 
+  }, 
+  {
+    name:'Toph',
+    element: 'Earth',
+    img: 'http://images4.wikia.nocookie.net/__cb20081225191856/avatar/images/2/2e/Toph.png',
+    title: 'Blind-Bandit' 
+  }, 
+  {
+    name:'Suki',
+    element: 'None',
+    img: 'https://comicvine1.cbsistatic.com/uploads/scale_super/11138/111385676/7019867-suki.jpg',
+    title: 'Kyoshi-Warrior' 
+  }, 
+  {
+    name:'Appa',
+    element: 'Air',
+    img: 'https://preview.redd.it/nx50st82y8351.png?auto=webp&s=f31cdd2db86f73ee5245bf8462fa7df187119254',
+    title: 'Bison' 
+  }, 
+  {
+    name:'Momo',
+    element: 'Air',
+    img: 'https://pbs.twimg.com/media/Eb78V98WsAU4-4G.png',
+    title: 'Lemur' 
+  },
+];
