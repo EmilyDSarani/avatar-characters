f05efd3eb059a0821070ea5c5205302585c66b60
EmilyDSarani <emily.sarani@gmail.com>
2021-09-29 17:01:59 -0500

    passing tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,34 +28,100 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns avatar characters', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
-        },
+          id: 1,
+          name:'Aang',
+          element: 'Air',
+          img: 'https://i.insider.com/5ebdbc8c3ad8612a1c7aa143?width=856&format=jpeg',
+          title: 'Avatar',
+          owner_id: 1 
+        }, 
         {
-          'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
-        },
+          id: 2,
+          name:'Zuko',
+          element: 'Fire',
+          img: 'https://i.redd.it/jcyddoyzeo761.jpg',
+          title: 'Prince',
+          owner_id: 1 
+
+        }, 
         {
-          'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
-        }
+          id: 3,
+          name:'Katara',
+          element: 'Water',
+          img: 'http://pm1.narvii.com/6078/caabea330dc68f44a3f855606f897516ec582759_00.jpg',
+          title: 'Healer', 
+          owner_id: 1 
+        }, 
+        {
+          id: 4,
+          name:'Sokka',
+          element: 'None',
+          img: 'https://i.pinimg.com/originals/08/ca/e8/08cae8f53ebfd473b1b3133b0138d9cd.jpg',
+          title: 'Plan-Guy',
+          owner_id: 1  
+        }, 
+        {
+          id: 5,
+          name:'Toph',
+          element: 'Earth',
+          img: 'http://images4.wikia.nocookie.net/__cb20081225191856/avatar/images/2/2e/Toph.png',
+          title: 'Blind-Bandit',
+          owner_id: 1  
+        }, 
+        {
+          id: 6,
+          name:'Suki',
+          element: 'None',
+          img: 'https://comicvine1.cbsistatic.com/uploads/scale_super/11138/111385676/7019867-suki.jpg',
+          title: 'Kyoshi-Warrior',
+          owner_id: 1  
+        }, 
+        {
+          id: 7,
+          name:'Appa',
+          element: 'Air',
+          img: 'https://preview.redd.it/nx50st82y8351.png?auto=webp&s=f31cdd2db86f73ee5245bf8462fa7df187119254',
+          title: 'Bison',
+          owner_id: 1  
+        }, 
+        {
+          id: 8,
+          name:'Momo',
+          element: 'Air',
+          img: 'https://pbs.twimg.com/media/Eb78V98WsAU4-4G.png',
+          title: 'Lemur',
+          owner_id: 1  
+        },
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/avatar')
         .expect('Content-Type', /json/)
         .expect(200);
 
+      expect(data.body).toEqual(expectation);
+    });
+  
+    test('returns 1 avatar character', async() => {
+
+      const expectation = 
+        {
+          id: 1,
+          name:'Aang',
+          element: 'Air',
+          img: 'https://i.insider.com/5ebdbc8c3ad8612a1c7aa143?width=856&format=jpeg',
+          title: 'Avatar',
+          owner_id: 1 
+        };
+      const data = await fakeRequest(app)
+        .get('/avatar/1')
+        .expect('Content-Type', /json/);
+      // .expect(200);
+  
       expect(data.body).toEqual(expectation);
     });
   });
