d48dac0586d0b1fb3b73cd064e419a3a6109320f
EmilyDSarani <emily.sarani@gmail.com>
2021-09-30 18:58:38 -0500

    changed pictures, so had to check the testing

--- data/avatar.js
+++ data/avatar.js
@@ -2,49 +2,49 @@ module.exports = [
   {
     name:'Aang',
     element: 'Air',
-    img: 'https://i.insider.com/5ebdbc8c3ad8612a1c7aa143?width=856&format=jpeg',
+    img: 'https://www.pngitem.com/pimgs/m/233-2336335_transparent-aang-png-avatar-aang-png-download.png',
     title: 'Avatar' 
   }, 
   {
     name:'Zuko',
     element: 'Fire',
-    img: 'https://i.redd.it/jcyddoyzeo761.jpg',
+    img: 'https://www.pngitem.com/pimgs/m/375-3756861_zuko-avatar-png-transparent-png.png',
     title: 'Prince' 
   }, 
   {
     name:'Katara',
     element: 'Water',
-    img: 'http://pm1.narvii.com/6078/caabea330dc68f44a3f855606f897516ec582759_00.jpg',
+    img: 'https://www.nicepng.com/png/detail/157-1576012_katara-de-avatar-katara-transparent.png',
     title: 'Healer' 
   }, 
   {
     name:'Sokka',
     element: 'None',
-    img: 'https://i.pinimg.com/originals/08/ca/e8/08cae8f53ebfd473b1b3133b0138d9cd.jpg',
+    img: 'https://www.kindpng.com/picc/m/548-5484508_avatar-the-last-airbender-sokka-png-transparent-png.png',
     title: 'Plan-Guy' 
   }, 
   {
     name:'Toph',
     element: 'Earth',
-    img: 'http://images4.wikia.nocookie.net/__cb20081225191856/avatar/images/2/2e/Toph.png',
+    img: 'https://png.pngitem.com/pimgs/s/25-250115_aang-clipart-vector-toph-png-transparent-png.png',
     title: 'Blind-Bandit' 
   }, 
   {
     name:'Suki',
     element: 'None',
-    img: 'https://comicvine1.cbsistatic.com/uploads/scale_super/11138/111385676/7019867-suki.jpg',
+    img: 'https://static.wikia.nocookie.net/pure-good-wiki/images/1/14/Suki.png/revision/latest?cb=20210128205901',
     title: 'Kyoshi-Warrior' 
   }, 
   {
     name:'Appa',
     element: 'Air',
-    img: 'https://preview.redd.it/nx50st82y8351.png?auto=webp&s=f31cdd2db86f73ee5245bf8462fa7df187119254',
+    img: 'https://static.wikia.nocookie.net/p__/images/0/06/Appa.png/revision/latest?cb=20180813222453&path-prefix=protagonist',
     title: 'Bison' 
   }, 
   {
     name:'Momo',
     element: 'Air',
-    img: 'https://pbs.twimg.com/media/Eb78V98WsAU4-4G.png',
+    img: 'https://static.wikia.nocookie.net/p__/images/5/54/Tumblr_p3utnb393U1tyjd90o1_400.png/revision/latest?cb=20210227080156&path-prefix=protagonist',
     title: 'Lemur' 
   },
 ];
