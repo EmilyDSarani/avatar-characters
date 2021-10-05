ff7b0d711d01d003cabcd789e91aa11d38e5489a
EmilyDSarani <emily.sarani@gmail.com>
2021-10-05 16:02:47 -0500

    had to go back and add element_name because it was not rendering in fron-end, so had to make sure it was fine here. All the tests now work smh

--- data/avatar.js
+++ data/avatar.js
@@ -2,48 +2,56 @@ module.exports = [
   {
     name:'Aang',
     element_id: 2,
+    element_name: 'Air',
     img: 'https://www.pngitem.com/pimgs/m/233-2336335_transparent-aang-png-avatar-aang-png-download.png',
     title: 'Avatar' 
   }, 
   {
     name:'Zuko',
     element_id: 1,
+    element_name: 'Fire',
     img: 'https://www.pngitem.com/pimgs/m/375-3756861_zuko-avatar-png-transparent-png.png',
     title: 'Prince' 
   }, 
   {
     name:'Katara',
     element_id: 3,
+    element_name: 'Water',
     img: 'https://www.nicepng.com/png/detail/157-1576012_katara-de-avatar-katara-transparent.png',
     title: 'Healer' 
   }, 
   {
     name:'Sokka',
     element_id: 5,
+    element_name: 'Non-Bender',
     img: 'https://www.kindpng.com/picc/m/548-5484508_avatar-the-last-airbender-sokka-png-transparent-png.png',
     title: 'Plan-Guy' 
   }, 
   {
     name:'Toph',
     element_id: 4,
+    element_name: 'Earth',
     img: 'https://png.pngitem.com/pimgs/s/25-250115_aang-clipart-vector-toph-png-transparent-png.png',
     title: 'Blind-Bandit' 
   }, 
   {
     name:'Suki',
     element_id: 5,
+    element_name: 'Non-Bender',
     img: 'https://www.pngkit.com/png/full/157-1575591_png-avatar-the-last-airbender-image-freeuse-avatar.png',
     title: 'Kyoshi-Warrior' 
   }, 
   {
     name:'Appa',
     element_id: 2,
+    element_name: 'Air',
     img: 'https://64.media.tumblr.com/ac50fb0c75b02757620b46478077b413/tumblr_p27negJA0A1tyjd90o1_400.png',
     title: 'Bison' 
   }, 
   {
     name:'Momo',
     element_id: 2,
+    element_name: 'Air',
     img: 'https://i.pinimg.com/originals/be/31/83/be31836efa6e88f094558feac39c2cf4.png',
     title: 'Lemur' 
   },
